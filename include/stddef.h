f9f98a76e0bdedbb70e9ad17206127089a4ea8c8
ser0ja <lanstyle@rambler.ru>
2017-05-13 22:40:25 +0300

    22:40 13.05.2015

--- include/stddef.h
+++ include/stddef.h
@@ -1,4 +1,2 @@
 // 7.19 Common definitions
 #pragma once
-
-typedef unsigned short wchar_t;
--- main.c
+++ main.c
@@ -1,16 +1,19 @@
+#define _WIN64 1
+//#include "main.h"
+//#include "winapi/windows.h"
 #include <stdio.h>
-#include <stddef.h>
-#include <stdbool.h>
-
-int main(int argc, char *argv[]) {
+ 
+ int main(int argc, char *argv[]) {
 	
 	char charvar = 'a';
-	char *charptr = (void *) 0x000000000060FD9F;
-	printf_s("address of charvar = %p (%hu) -> %c\n", (void *) &charvar, sizeof(charvar), *charptr);
+	//char *charptr = (void *) 0x000000000060FD9F;
+	printf_s("address of charvar = %p (%hu)\n", (void *) &charvar, sizeof(charvar));
+	//printf_s("address of charvar = %p (%hu) -> %c\n", (void *) &charvar, sizeof(charvar), *charptr);
 
 	int intvar = 0;
-	int *intptr = (void *) 0x000000000060FD8C;
-	printf_s("address of intvar = %p (%hu) -> %hhd\n", (void *) &intvar, sizeof(intvar), *intptr);
+	//int *intptr = (void *) 0x000000000060FD8C;
+	printf_s("address of intvar = %p (%hu)\n", (void *) &intvar, sizeof(intvar));
+	//printf_s("address of intvar = %p (%hu) -> %hhd\n", (void *) &intvar, sizeof(intvar), *intptr);
 	
 	return 0;
 }
