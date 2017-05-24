613927739b839499599320d7e869a99066d29549
ser0ja <lanstyle@rambler.ru>
2017-05-25 00:25:24 +0300

    00:25 25.05.2017

--- main.c
+++ main.c
@@ -1,45 +1,45 @@
-#pragma comment(lib, "winmm.lib")
-#pragma comment(lib, "user32.lib")
 
-#ifndef _WIN64
-#define _WIN64
-#endif
-#ifndef UNICODE
-#define UNICODE
-#endif
+#define _WINDOWS 1
+#define _WIN64 1
+#define UNICODE 1
+#define _WIN32_WINNT 0x0A00 // windows 10
+#define WINVER 0x0A00 // windows 10
 
 //#include "main.h"
 //#include "win.h"
 
-#define CALLBACK __stdcall
+#undef __cdecl
+#define __stdcall
 #define WINAPI __stdcall
+#define NULL ((void *)0)
 
 struct HINSTANCE__ { int unused; };
 typedef struct HINSTANCE__ *HINSTANCE;
+typedef void *HWND;
 
 typedef int INT;
+typedef unsigned int UINT;
 typedef char CHAR;
-typedef CHAR *LPSTR;
 typedef unsigned short wchar_t;
 typedef wchar_t WCHAR;
-typedef WCHAR *PWSTR;
+typedef WCHAR *LPWSTR;
+typedef const WCHAR *LPCWSTR;
 
 #ifdef UNICODE
-
+INT WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, INT);
+INT WINAPI MessageBoxW(HWND, LPCWSTR, LPCWSTR, UINT);
 #define __T(x) L ## x
-#define _tmain wmain
+#define _T(x) __T(x)
+#define _tmainCRTStartup wWinMainCRTStartup
 #define _tWinMain wWinMain
-
+#define _runtwinmain _runwwinmain
+#define MessageBox MessageBoxW
 #else // !UNICODE
-	
-#define __T(x) x
-#define _tmain main
-#define _tWinMain WinMain
-
 #endif // UNICODE
 
-		
-int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, INT nCmdShow){
-	//MessageBox(NULL, TEXT("Привет, мир!"), TEXT("Сообщение"), MB_OK);
+#define MB_OK 0x00000000L
+
+INT WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, INT nShowCmd){
+	MessageBox(NULL, _T("Привет, мир!"), _T("Сообщение"), MB_OK);
 	return 0;
 }
